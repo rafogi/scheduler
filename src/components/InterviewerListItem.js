de8013e8f6798f0b0b9fc04e4c8a92d2a3835f52
rafogi <raffycordero2001@yahoo.ca>
2020-09-28 22:52:15 -0700

    modified stories for selected and clickable interviewListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,15 +9,19 @@ import "components/InterviewerListItem.scss";
 // setInterviewer:function - sets the interviewer upon selection
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classnames ('interviewers__item', {
+    'interviewers__item--selected':props.selected
+  })
+
 
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
         alt="Sylvia Palmer"
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 
