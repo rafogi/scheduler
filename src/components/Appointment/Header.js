9c949e15650508097d3d0c5985d0b879e173aa6d
rafogi <raffycordero2001@yahoo.ca>
2020-09-29 14:44:56 -0700

    created for application index.js styles.scss empty.js scss header js scss

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classnames from 'classnames';
+import "./Header.scss";
+
+const Header = (props) => {
+  return (
+<header className="appointment__time">
+  <h4 className="text--semi-bold">{props.time}</h4>
+  <hr className="appointment__separator" />
+</header>
+  );
+};
+
+export default Header;
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.scss'
 
-const Appointment = () => {
+const Appointment = (props) => {
   return (
 <article className="appointment"></article>
   );
