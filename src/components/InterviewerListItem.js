f17370871a0a8863e4e605c65000ecc95c54d285
rafogi <raffycordero2001@yahoo.ca>
2020-09-28 22:28:11 -0700

    created InterviewerListItem file and .scss

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classnames from 'classnames';
+import "components/InterviewerListItem.scss";
+
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - to determine if an interview is selected or not
+// setInterviewer:function - sets the interviewer upon selection
+
+export default function InterviewerListItem(props) {
+
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+
+
+
+}
