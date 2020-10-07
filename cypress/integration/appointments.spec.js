9f8dfa295fb0ecec4eaff965d231c5b744192eee
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-10-07 20:09:11 +0000

    test navigation using cypress

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,29 @@
+describe("Appointments", () => {
+  it("should book an interview", () => {
+
+    // Visits the root of our web server
+    cy.visit("/");
+    // Clicks on the "Add" button in the second appointment
+    // Enters their name
+    // Chooses an interviewer
+    // Clicks the save button
+    // Sees the booked appointment
+  });
+  it("should edit an interview", () => {
+    
+    //Visits the root of our web server
+    cy.visit("/");
+    //Clicks the edit button for the existing appointment
+    //Changes the name and interviewer
+    //Clicks the save button
+    //Sees the edit to the appointment
+  });
+  it("should cancel an interview", () => {
+    
+    // Visits the root of our web server
+    cy.visit("/");
+    // Clicks the delete button for the existing appointment
+    // Clicks the confirm button
+    // Sees that the appointment slot is empty
+  });
+})
